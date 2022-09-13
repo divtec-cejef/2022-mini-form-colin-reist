528be1b2c1205db7f3a695f9d7dee45f0c965f36
Colin Reist <colinreist04@gmail.com>
2022-09-13 11:05:07 +0200

    Fin du cours

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 /**
- * @author Steve Fallet
+ * @author Colin Reist
  * @since 2022.09.06
  */
 
 'use strict';
+
+const formulaire = document.querySelector('form');
+formulaire.addEventListener('submit', function (event) {
+    event.preventDefault(); // annule l'envoie du formulaire
+    alert('envoyé');
+});
+
+const listeBackground = document.getElementById('background');
+formulaire.addEventListener('change', function (event) {
+    event.preventDefault(); // annule l'envoie du formulaire
+    alert(listeBackground.value);
+    document.body.style.backgroundImage = `url(./img/backgrounds/${listeBackground.value})`
+});
 
 console.log('Test liaison JS');
